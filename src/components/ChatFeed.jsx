4e99a8ec0c8435e8ad90fa95c8393e081f623427
Monroe Gollaher <mgollaher92@gmail.com>
2021-04-01 23:52:57 -0600

    other user message component

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -24,7 +24,10 @@ const ChatFeed = (props) => {
               : <TheirMessage message={ message } lastMessage={ messages[lastMessageKey] }/>
             }
           </div>
-          <div className="read-receipts" style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px' }}>
+          <div 
+            className="read-receipts" 
+            style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px' }}
+          >
             read receipts
           </div>
         </div>
